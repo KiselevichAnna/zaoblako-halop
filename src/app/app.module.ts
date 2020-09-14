610c6d4d3181244011de42ba72fec2b377273e0e
Anna Kiselevich <kaletyuk@yandex.ru>
2020-09-14 22:31:29 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { DashboardComponent} from './components/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+  ],
+  imports: [ BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
